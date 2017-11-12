2304a16b2865e2433c90f455cae621f592108fbf
Alexandre Buisine <alexandrejabuisine@gmail.com>
2017-11-12 17:30:53 +0100

    now wraps miner

--- resources/usr/local/bin/NVMLCollector.py
+++ resources/usr/local/bin/NVMLCollector.py
@@ -1,17 +1,4 @@
-#!/usr/bin/python
-
-"""
-Pushes nVidia GPU metrics to a Prometheus Push gateway for later collection.
-"""
-
-import argparse
 import logging
-import time
-import platform
-
-import socket
-import sys
-import json
 
 from prometheus_client import CollectorRegistry, Gauge, Counter, push_to_gateway
 from prometheus_client import start_http_server, core
@@ -19,40 +6,7 @@ from prometheus_client.core import GaugeMetricFamily, CounterMetricFamily, REGIS
 
 from pynvml import *
 
-import MinerCollector
-
-log = logging.getLogger('prometheus-nvidia-exporter')
-
-def _create_parser():
-	parser = argparse.ArgumentParser(description='nVidia GPU Prometheus Metrics Exporter')
-	parser.add_argument('--verbose',
-						help='Turn on verbose logging',
-						action='store_true')
-
-	parser.add_argument('-u', '--update-period',
-						help='Period between calls to update metrics, '
-							 'in seconds. Defaults to 30.',
-						default=30)
-
-	parser.add_argument('-g', '--gateway',
-						help='If defined, gateway to push metrics to. Should '
-							 'be in the form of <host>:<port>.',
-						default=None)
-
-	parser.add_argument('-p', '--port',
-						help='If non-zero, port to run the http server',
-						type=int,
-						default=0)
-
-	parser.add_argument('-mh', '--miner-host',
-						help='miner API hostname',
-						default="localhost")
-
-	parser.add_argument('-mp', '--miner-port',
-						help='miner API tcp port',
-						type=int,
-						default=3333)
-	return parser
+log = logging.getLogger('prometheus-nvml-exporter')
 
 class NVMLCollector(object):
 
@@ -64,6 +18,7 @@ class NVMLCollector(object):
 		self.prefix_s	= 'NVML '
 
 	def collect(self):
+		log.info('collecting')
 		try:
 			log.debug('Querying for clocks information...')
 			metric = GaugeMetricFamily(self.prefix + 'clock_gpu_hz', self.prefix_s + "GPU clock", labels=self.labels.keys())
@@ -101,51 +56,3 @@ class NVMLCollector(object):
 			yield metric
 		except Exception as e:
 			log.warning(e, exc_info=True)
-
-def main():
-	parser = _create_parser()
-	args = parser.parse_args()
-	logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
-
-	try:
-		log.debug('Initializing NVML...')
-		nvmlInit()
-
-		log.info('Started with nVidia driver version = %s', 
-		nvmlSystemGetDriverVersion())
-
-		log.debug('Obtaining device ...')
-		nvml_device = nvmlDeviceGetHandleByIndex(0)
-
-		log.debug('Querying for ID information...')
-		labels = {
-			'gpu_uuid':		nvmlDeviceGetUUID(nvml_device),
-			'pci_bus_id':	nvmlDeviceGetPciInfo(nvml_device).busId
-			}
-		log.debug('Device is %s', labels['gpu_uuid'])
-
-		REGISTRY.register(NVMLCollector(labels, nvml_device))
-		REGISTRY.register(MinerCollector.MinerCollector(labels, args.miner_host, args.miner_port))
-
-		if args.port:
-			log.debug('Starting http server on port %d', args.port)
-			start_http_server(args.port)
-			log.info('HTTP server started on port %d', args.port)
-
-		while True:
-			if args.gateway:
-				log.debug('Pushing metrics to gateway at %s...', args.gateway)
-				hostname = platform.node()
-				push_to_gateway(args.gateway, job=hostname, registry=REGISTRY)
-				log.debug('Push complete.')
-
-			time.sleep(args.update_period)
-
-	except Exception as e:
-		log.error('Exception thrown - %s', e, exc_info=True)
-	finally:
-		nvmlShutdown()
-   
-
-if __name__ == '__main__':
-	main()
---
+++ resources/usr/local/bin/docker-entrypoint.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python
+
+"""
+Pushes nVidia GPU metrics to a Prometheus Push gateway for later collection.
+"""
+
+import argparse
+import logging
+import time
+import platform
+
+import socket
+import os
+import json
+import importlib
+
+from prometheus_client import CollectorRegistry, Gauge, Counter, push_to_gateway
+from prometheus_client import start_http_server, core
+from prometheus_client.core import GaugeMetricFamily, CounterMetricFamily, REGISTRY
+
+from pynvml import *
+
+import NVMLCollector
+
+log = logging.getLogger('miner-wrapper')
+
+def _create_parser():
+	parser = argparse.ArgumentParser(description='nVidia GPU Prometheus Metrics Exporter')
+	parser.add_argument('--verbose',
+						help='Turn on verbose logging',
+						action='store_true')
+
+	parser.add_argument('-u', '--update-period',
+						help='Period between calls to update metrics, '
+							 'in seconds. Defaults to 30.',
+						default=30)
+
+	parser.add_argument('-g', '--gateway',
+						help='If defined, gateway to push metrics to. Should '
+							 'be in the form of <host>:<port>.',
+						default=None)
+
+	parser.add_argument('-cp', '--collector-port',
+						help='If non-zero, port to run the collector http server',
+						type=int,
+						default=0)
+
+	parser.add_argument('-mm', '--miner-module',
+						help='miner python module',
+						required=True)
+
+	parser.add_argument('-mh', '--miner-host',
+						help='miner API hostname',
+						default="localhost")
+
+	parser.add_argument('-mp', '--miner-port',
+						help='miner API tcp port',
+						type=int,
+						required=True)
+	return parser
+
+def collect(args, Miner):
+	log.debug('initializing NVML...')
+	nvmlInit()
+
+	log.info('started with nVidia driver version = %s', 
+	nvmlSystemGetDriverVersion())
+
+	log.debug('obtaining device ...')
+	nvml_device = nvmlDeviceGetHandleByIndex(0)
+
+	log.debug('querying for ID information...')
+	labels = {
+		'gpu_uuid':		nvmlDeviceGetUUID(nvml_device) ,
+		'pci_bus_id':	nvmlDeviceGetPciInfo(nvml_device).busId
+		}
+	log.debug('device is %s', labels['gpu_uuid'])
+
+	REGISTRY.register(NVMLCollector.NVMLCollector(labels, nvml_device))
+	REGISTRY.register(Miner.MinerCollector(labels, args.miner_host, args.miner_port))
+	if args.collector_port:
+		log.debug('starting http server on port %d', args.collector_port)
+		start_http_server(args.collector_port)
+		log.info('HTTP server started on port %d', args.collector_port)
+
+	while True:
+		if args.gateway:
+			log.debug('pushing metrics to gateway at %s...', args.gateway)
+			hostname = platform.node()
+			push_to_gateway(args.gateway, job=hostname, registry=REGISTRY)
+			log.debug('push complete.')
+
+		time.sleep(args.update_period)
+
+def main():
+	parser = _create_parser()
+	args = parser.parse_args()
+	logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
+
+	Miner = importlib.import_module(args.miner_module, package=None)
+
+	try:
+		pid = os.fork()
+
+		if ( pid == 0 ):
+			log.info('collect as a child process %d'%os.getpid())
+			collect(args, Miner)
+
+		Miner.launch(args)
+
+	except Exception as e:
+		log.error('Exception thrown - %s', e, exc_info=True)
+	finally:
+		nvmlShutdown()
+   
+
+if __name__ == '__main__':
+	main()
