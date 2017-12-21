c0e2f4fe7b3fea2fa6a522b911599810d6b39fda
wes <wes@home.local>
2017-12-21 06:14:34 -0600

    Updates to build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 VERSION_LIST=(384.90)
 
 for version in $VERSION_LIST; do
-  docker build -t nvidia:$version -f Dockerfile.$version .
-  docker push nvidia:$version
+  docker build -t wesparish/nvidia:$version -f Dockerfile.$version .
+  docker push wesparish/nvidia:$version
 done
