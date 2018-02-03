aedbfd13aea564c439b914bf21eda8c9c40889cf
wes <wes@home.local>
2018-02-03 07:20:57 -0600

    Adding Ubuntu 16.04 support for 384.90

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-VERSION_LIST=(384.90)
+#VERSION_LIST=(384.90 384.98)
+VERSION_LIST=(384.90-ubuntu1604)
 
-for version in $VERSION_LIST; do
+for version in ${VERSION_LIST[@]}; do
+  echo "*** BUILDING $version ***"
   docker build -t wesparish/nvidia:$version -f Dockerfile.$version .
   docker push wesparish/nvidia:$version
 done
